818bb1924e7d04e5783e930a2413ebdf79316c6c
Florian Dohmann <florian@dohmann-web.de>
2013-04-06 21:00:30 +0200

    PPE prototype one ready to go!

--- ra/ppe/ppe.py
+++ ra/ppe/ppe.py
@@ -10,57 +10,41 @@ For using the module install the pattern library via "pip install pattern"
 from pattern.web import Spider, DEPTH, BREADTH, FIFO, LIFO, URL, plaintext
 from pattern.db import Datasheet
 from pattern.vector import Document, Corpus, Bayes
-from nltk.tokenize import RegexpTokenizer
-from nltk.classify import NaiveBayesClassifier as nbc
+import urllib2
 import re, os
 
-class NaiveBayesClassifier():
-
-    def __init__(self, training_data1, training_data2):
-        self._training_set = self._create_taining_set(training_data1, training_data2)
-        self._classifier = nbc.train(self._training_set)
-
-    def _create_taining_set(self, training_data1, training_data2):
-    	training_set = []
-       	training_text = open(training_data1).read()
-        words = tokenize(training_text)
-        training_set += [(dict([(word, True)]), "policy") for word in words]
-
-        training_text = open(training_data2).read()
-        words = tokenize(training_text)
-        training_set += [(dict([(word, True)]), "no_policy") for word in words]
-
-    	return training_set
-
-    def classify(self, text):
-        result = {}
-        prob_dist = self._classifier.prob_classify(self._create_dict(text))
-        for label in self._classifier.labels():
-            result[label] = prob_dist.prob(label)
-        return result
-
-    def _create_dict(self, text):
-        words = tokenize(text)
-        return dict([(word, True) for word in words])
 
 def classifyPolicy(content):
 	'''
 	gets a pre-qualified html content
-	and returns if it contains a policy or not 
+	and returns if it contains a policy or not.
+	TODO: This function needs to be finished
+	The trainingset needs to be modified
 	'''
-	data = Datasheet.load("policy.corpus.csv")
+	# The corpus does not have an empty line 
+	# between the end of a policy and the new 
+	# policy
+	data = Datasheet.load("policy_corpus.csv")
 	documents = []
 	for score, message in data:
 		document = Document(message, type=int(score) > 0)
 		documents.append(document)
 	corpus = Corpus(documents)
-	print "number of documents", len(corpus)
+	print "number of documents:", len(corpus)
+	print "number of words:", len(corpus.vector)
+	print "number of words (average):", sum(len(d.terms) for d in corpus.documents) / float(len(corpus))
+	print
+	classifier = Bayes()
+	for document in corpus:
+		classifier.train(document)
+	print "Is it a policy?", classifier.classify(content)
+	return True
 
 def checkIfPolicy(url):
 	'''
 	gets the crawled url
 	returns True if the url is a potential
-	candidate for uncluding a policy and False if not
+	candidate for having a policy and False if not
 	'''
 	flags = (re.UNICODE | re.MULTILINE | re.IGNORECASE)
 	policyExp = re.compile(ur"policy",flags)
@@ -71,9 +55,25 @@ def checkIfPolicy(url):
 		return False
 
 def cleanHtml(html):
+	'''
+	gets the html document as a string and
+	and retrurns the cleaned raw text
+	'''
 	# check pattern documentation for modification (http://www.clips.ua.ac.be/pages/pattern-web#plaintext)
 	text = plaintext(html)
 	return text
+
+def deleteBoilerplates(url):
+	'''
+	gets the link to a website,
+	sends it to the boilerplate API,
+	and returns the cleaned html from
+	the http response 
+	'''
+	# check pattern documentation for modification (http://www.clips.ua.ac.be/pages/pattern-web#plaintext)
+	req = urllib2.Request('http://boilerpipe-web.appspot.com/extract?url='+url)
+	result = urllib2.urlopen(req)
+	return result.read()
 
 
 class ppSpider(Spider):
@@ -82,8 +82,23 @@ class ppSpider(Spider):
         # Do the pre filtering via Regular Expression
         if checkIfPolicy(link.url) == True:
         	print "Found a policy in", link.url
-        	cleanPage = cleanHtml(source)
-        	#classifyPolicy(source)
+        	print source
+        	print 10 * "----"
+        	noBoilerplates = deleteBoilerplates(link.url)
+        	print noBoilerplates
+        	print 10 * "----"
+        	if noBoilerplates == "":
+        		print "Problems with the Boilerpipe API"
+        	else:
+        		cleanPage = cleanHtml(noBoilerplates)
+        		print cleanPage
+        		print 10 * "----"
+        		if classifyPolicy(cleanPage) is True:
+        			# loadToDatabase() # needs to be implemented
+        			print 20 * "-"
+        			print "Info: No policy saved to DB because classifier/trainingset needs to be optimized."
+        			print 20 * "-"
+
         else:
   			print "No Policy found"
 
@@ -98,20 +113,13 @@ def extractPolicies(urls):
 		ppSpiderling.crawl(cached=False)
 
 
-def tokenize(text, regex=u'[a-zA-ZäöüÄÖÜß0-9]+', tolower=True):
-    tokenizer = RegexpTokenizer(regex, flags=re.UNICODE)
-    if tolower:
-        text = text.lower()
-    tokens = tokenizer.tokenize(text)
-    return tokens
-
-
 if __name__ == '__main__':
-	# test set of policies
+	# test set of start urls for running the script 
+	# directly in pyton 
 	urls = ['http://www.google.com',
 	'http://www.facebook.com', 
 	'http://www.youtube.com', 
 	'http://www.yahoo.com', 
 	'http://en.wikipedia.org', 
-	'http://www.amazon.com']
+	'http://www.amazon.com'
 	extractPolicies(urls)
